774d7c6ca7d71cf37aef4fcbd2f4aa7b8907e7ef
aliencyy <kelsyamirah62@gmail.com>
2022-11-01 19:30:24 +0700

    membuat template MainLaundry.java
    membuat template awal

---
+++ Source Code/MainLaundryTime.java
@@ -0,0 +1,10 @@
+/**
+ * @author Kelsy Amirah dan Najla Raihana Kamila
+ * @version 01-11-2022
+ */
+
+public class MainLaundryTime {
+    public static void main(String[] args) {
+        
+    }
+}

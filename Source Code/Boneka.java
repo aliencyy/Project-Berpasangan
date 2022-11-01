6ab5b9bacd8e8a68c89dd03e744fda2cff55520e
aliencyy <105506596+aliencyy@users.noreply.github.com>
2022-11-01 19:48:21 +0700

    menambahkan file Boneka.java
    membuat template awal untuk Boneka.java

---
+++ Source Code/Boneka.java
@@ -0,0 +1,34 @@
+public class Boneka extends PaketLaundry{
+    @Override
+    public String Paket1(){
+        return "mencuci";
+    }
+
+    @Override
+    public String Paket2() {
+        return "menggosok";
+    }
+
+    @Override
+    public String Paket3() {
+        return "mencuci dan menggosok";
+    }
+
+    @Override
+    public double hargaPaket1() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double hargaPaket2() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double hargaPaket3() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}

54a3d09cf8a35efa9557ba957722b00b29e851c7
aliencyy <105506596+aliencyy@users.noreply.github.com>
2022-11-04 22:02:11 +0700

    Update MainLaundryTime.java

--- Source Code/MainLaundryTime.java
+++ Source Code/MainLaundryTime.java
@@ -5,6 +5,17 @@
 
 public class MainLaundryTime {
     public static void main(String[] args) {
-        
+        int pilihan; 
+        Scanner ldr = new Scanner(System.in);
+
+        System.out.println("Selamat datang di Laundry Time!");
+        System.out.println("Berikut paket yang dapat dipilih");
+        System.out.println("+------------------------------------------------------------------------------------------------------------+");
+        System.out.println("|\tPakaian\t\t|\tSelimut\t\t|\tTas dan Sepatu\t\t|\t   Boneka   \t     |");
+        System.out.println("+------------------------------------------------------------------------------------------------------------+");
+        System.out.println("|      PAKET 1 \t\t|\tPAKET 1\t\t|\t   PAKET 1\t\t|\t   PAKET 1\t     |");
+        System.out.println("|      Mencuci\t\t|\tMencuci\t\t|\t Dry Cleaning\t\t|\t Dry Cleaning\t     |");
+        System.out.println("|   Harga: Rp5.000\t|     Harga: 6.000\t|\tHarga: 15.000\t\t|\tHarga: 15.000\t     |");
+        System.out.println("+------------------------------------------------------------------------------------------------------------+");
     }
 }

8febe62f7ca8dbfba4145c7a24c93b3c2d98e738
najlarhana <101654857+najlarhana@users.noreply.github.com>
2022-11-04 22:06:10 +0700

    Update MainLaundryTime.java

--- Source Code/MainLaundryTime.java
+++ Source Code/MainLaundryTime.java
@@ -17,5 +17,15 @@ public class MainLaundryTime {
         System.out.println("|      Mencuci\t\t|\tMencuci\t\t|\t Dry Cleaning\t\t|\t Dry Cleaning\t     |");
         System.out.println("|   Harga: Rp5.000\t|     Harga: 6.000\t|\tHarga: 15.000\t\t|\tHarga: 15.000\t     |");
         System.out.println("+------------------------------------------------------------------------------------------------------------+");
+         System.out.println("|      PAKET 2 \t\t|\tPAKET 2\t\t|\t   PAKET 2\t\t|\t   PAKET 2\t     |");
+        System.out.println("|      Menggosok\t|\tMenggosok\t|\t    Vakum\t\t|\t    Vakum\t     |");
+        System.out.println("|   Harga: Rp6.000\t|     Harga: 10.000\t|\tHarga: 10.000\t\t|\tHarga: 10.000\t     |");
+        System.out.println("+------------------------------------------------------------------------------------------------------------+");
+        System.out.println("|      PAKET 3 \t\t|\tPAKET 3\t\t|\t   PAKET 3\t\t|\t   PAKET 3\t     |");
+        System.out.println("| Mencuci & Menggosok\t|   Mencuci & Menggosok\t|    Dry Cleaning & Vakum\t|    Dry Cleaning & Vakum    |");
+        System.out.println("|   Harga: Rp10.000\t|     Harga: 15.000\t|\tHarga: 20.000\t\t|\tHarga: 20.000\t     |");
+        System.out.println("+------------------------------------------------------------------------------------------------------------+");
+        System.out.print("Masukkan paket yang anda inginkan : ");
+        pilihan = ldr.nextInt();
     }
 }

81e220433dcb75b59f95f45d18d841c9e10acf6f
aliencyy <105506596+aliencyy@users.noreply.github.com>
2022-11-09 09:50:26 +0700

    memperbarui PaketLaundry.java
    menambahkan komen (java doc)

--- Source Code/PaketLaundry.java
+++ Source Code/PaketLaundry.java
@@ -1,11 +1,13 @@
 /**
+ * CLass PaketLaundry merupakan class abstract
  * 
+ * @author Kelsy Amirah dan Najla Raihana Kamila
+ * @version 08-11-2022
  */
 
 public abstract class PaketLaundry {
     
     // inisialisasi atribut
-    protected String namaPelanggan = "";
     public abstract String Paket1(); // mencuci
     public abstract String Paket2(); // menggosok
     public abstract String Paket3(); // menggosok + mencuci
